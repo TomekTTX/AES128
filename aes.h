9c051cca73426b6fdd5bd2eb548d834af0614039
TomekTTX <tomekttx@gmail.com>
2020-05-30 21:32:45 +0200

    Removed obsolete function headers

--- aes.h
+++ aes.h
@@ -1,9 +1,6 @@
 #pragma once
 #include <stdio.h>
 
-//void AES_Encrypt(unsigned char *data, unsigned int size, unsigned char *key);
-//void AES_Decrypt(unsigned char *data, unsigned int size, unsigned char *key);
-
 void AESEncryptEcb(FILE *in, FILE *out, unsigned char *key);
 void AESDecryptEcb(FILE *in, FILE *out, unsigned char *key);
 
